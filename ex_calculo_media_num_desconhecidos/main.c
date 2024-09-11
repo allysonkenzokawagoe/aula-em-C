c145c5ba073fc5e19875b878a47a821fa23b5041
Allyson Kenzo Kawagoe <81180394+allysonkenzokawagoe@users.noreply.github.com>
2024-09-11 20:58:16 -0300

    Update main.c

--- ex_calculo_media_num_desconhecidos/main.c
+++ ex_calculo_media_num_desconhecidos/main.c
@@ -4,19 +4,20 @@
 int main()
 {
 
-    int num, media = 0, i = 0;
+    int num, i = 0, soma = 0;
+    float media;
 
     do {
         printf("Digite um numero inteiro: ", num);
         scanf("%d", &num);
-        media += num;
-        i++;
-    } while(num > 0);
+        soma += num;
+        if(num != 0) {
+            i++;
+        }
+    } while(num != 0);
 
-    if(num == 0) {
-        media = media / (i - 1);
-        printf("A media dos numeros eh de: %d", media);
-    }
+        media = soma / i;
+        printf("A media dos numeros eh de: %.2f", media);
 
     return 0;
 }
